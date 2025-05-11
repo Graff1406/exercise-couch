40cb408ab6a92c4e45e15769fea16b1a3690d288
avtandil <shainidze@gmail.com>
2025-05-11 17:22:35 +0300

    add startSpeach functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import ExerciseForm from "./components/ExerciseForm.vue";
-import { ref, onMounted, computed } from "vue";
+import { ref, watch, onMounted, computed } from "vue";
 import {
   VCard,
   VCardText,
@@ -22,7 +22,6 @@ interface Exercise {
   firstPhase: string;
   audioCues: string;
   sets: number;
-  completedSets: number;
   pause: number;
   countdownDuration: number;
   gifUrl: string;
@@ -37,20 +36,33 @@ interface Exercise {
   selectedForPlayer: boolean;
 }
 
+
 const backgroundAudio = ref<HTMLAudioElement | null>(null);
 
 const showForm = ref(false);
 const exercises = ref<Exercise[]>([]);
-
-const currentExerciseIndex = ref(0);
+const isPause = ref(false);
 
 const playerState = ref<PlayerState>("idle");
 const editExercise = ref<Partial<Exercise>>({});
 const groupExercises = ref<number[]>([]);
 const editMode = ref(false);
+const exerciseInProgress = ref<Exercise | null>(null);
+const utterance = new SpeechSynthesisUtterance();
 
 const countdown = ref(0);
+const currentIndex = ref(0);
 const countdownInterval = ref<ReturnType<typeof setInterval> | null>(null);
+
+const setCurrentExercise = () => {
+  if (currentIndex.value < exercises.value.length) {
+    exerciseInProgress.value = exercises.value[currentIndex.value];
+    currentIndex.value++;
+  } else {
+    // Все упражнения пройдены
+    exerciseInProgress.value = null;
+  }
+};
 
 const loadExercises = () => {
   const storedExercises = localStorage.getItem("exercises");
@@ -93,108 +105,47 @@ const resetCountdown = () => {
 
 const startMelody = () => {
   if (backgroundAudio.value) {
-    backgroundAudio.value.src = "melodies/melody_1.mp3";
+    backgroundAudio.value.src = 'melodies/melody_1.mp3';
     backgroundAudio.value.volume = 0.1;
     backgroundAudio.value.play();
   }
 };
 
-const playerText = ref("Готов к запуску");
+const startSpeach = (text: string) => {
+  console.log("Начало выполнения startSpeach: ", text)
 
-const updatePlayerText = () => {
-  const selected = exercises.value.filter(
-    (exercise) => exercise.selectedForPlayer
-  );
-  if (selected.length > 0 && currentExerciseIndex.value < selected.length) {
-    if (countdown.value > 0) {
-      // Display the name of the current exercise while the timer is running
-      playerText.value = selected[currentExerciseIndex.value].exerciseName;
-    } else if (currentExerciseIndex.value < selected.length) {
-      // Display "Перерыв" when the timer reaches zero between exercises
-      playerText.value = "Перерыв";
-    } else {
-      // Display a message when all exercises are completed
-      playerText.value = "Тренировка завершена!";
-    }
-  } else {
-    playerText.value = "Выберите упражнения для начала";
-  }
-};
-
-const startSpeach = (index: number) => {
+  
   const selected = exercises.value.filter(
     (exercise) => exercise.selectedForPlayer
   );
   if (selected.length === 0) {
     return;
   }
+  
+  utterance.text = text;
 
-  currentExerciseIndex.value = index;
-  updatePlayerText();
-
-  const utterance = new SpeechSynthesisUtterance(
-    `Упражнение: ${selected[index].exerciseName}. ${selected[index].repetitions} повторений.`
-  );
-  utterance.rate = 1.4;
-
-  let repetitionCount = 0;
-
-  utterance.onend = () => {
-    repetitionCount++;
-
-    if (repetitionCount <= selected[index].repetitions) {
-      // Continue with repetitions
-      if (repetitionCount % 2 !== 0) {
-        utterance.text = "Вверх";
-        utterance.rate = 1.5;
-      } else {
-        utterance.text = "Вниз";
-        utterance.rate = 0.5;
-      }
-      speechSynthesis.speak(utterance);
-    } else {
-      // Move to the next exercise after repetitions are done
-      if (index < selected.length) {
-        // Display "Перерыв" before the next exercise starts
-        playerText.value = "Перерыв";
-        // Start the next exercise after a pause
-        setTimeout(() => {
-          startSpeach(index + 1);
-        }, selected[index].pause);
-      } else {
-        // All exercises completed
-        resetPlayer();
-      }
-    }
-  };
-
-  // Start the countdown for the current exercise duration (including repetitions and sets)
-  countdown.value =
-    ((selected[index].concentricSpeed + selected[index].eccentricSpeed) *
-      selected[index].repetitions *
-      selected[index].sets) /
-    1000;
-  startCountdown();
   speechSynthesis.speak(utterance);
-};
+}
 
-const formattedCountdown = computed(() =>
-  formatTime(countdown.value || totalExercisesDuration.value)
-);
-const playerTextDisplay = computed(() => playerText.value);
+const formattedCountdown = computed(() => formatTime(countdown.value || totalExercisesDuration.value));
 
 const startPlayer = () => {
+
   playerState.value = "playing";
 
+  setCurrentExercise();
+
+  if (!exerciseInProgress.value) return;
+
   startMelody();
-  startSpeach(0); // Start with the first selected exercise
 };
 
 const countinuePlayer = () => {
+
   playerState.value = "playing";
 
   if (backgroundAudio.value) backgroundAudio.value.play();
-  speechSynthesis.resume();
+    speechSynthesis.resume();
 };
 
 const pausePlayer = () => {
@@ -204,7 +155,6 @@ const pausePlayer = () => {
     backgroundAudio.value.pause();
     speechSynthesis.pause();
   }
-  updatePlayerText();
   pauseCountdown();
 };
 
@@ -213,10 +163,11 @@ const resetPlayer = () => {
   resetCountdown();
   // Stop background audio and clear vocalizations
   if (backgroundAudio.value) {
-    backgroundAudio.value.pause();
+    backgroundAudio.value.load();
   }
-  updatePlayerText();
   speechSynthesis.cancel();
+  exerciseInProgress.value = null;
+  currentIndex.value = 0;
 };
 
 const calculateTotalExerciseTime = (
@@ -311,7 +262,9 @@ const totalExercisesDuration = computed(() => {
 });
 
 const isStartButtonDisabled = computed(() => {
-  return !exercises.value.some((exercise) => exercise.selectedForPlayer);
+ return !exercises.value.some(
+    (exercise) => exercise.selectedForPlayer
+ );
 });
 
 const isPlayerStarted = computed(() => {
@@ -333,6 +286,88 @@ onMounted(() => {
     exercises.value = [];
     groupExercises.value = [];
   }
+});
+
+watch(exerciseInProgress, () => {
+  if (!exerciseInProgress.value) return;
+  let movmentUp = true;
+
+  function setMovmentDirection () {
+    console.log(movmentUp)
+    startSpeach(movmentUp ? 'Вверх' : 'Вниз');
+    utterance.onend = () => {
+      movmentUp = !movmentUp;
+      setMovmentDirection();
+    }
+  }
+
+  const startPreparationExerciseSpeach = () => {
+    utterance.onend = () => {
+      startSpeach("Три");
+      utterance.onend = () => {
+        startSpeach("Два");
+        utterance.onend = () => {
+          startSpeach("Один");
+          utterance.onend = () => {
+            startSpeach("Старт")
+            utterance.onend = () => {
+              startCountdown();
+              // startSpeach('Вверх');
+              setMovmentDirection();
+            }
+          }
+        }
+      }
+    }
+  };
+
+  const timeCurrentEx = ((exerciseInProgress.value.eccentricSpeed + exerciseInProgress.value.concentricSpeed) * exerciseInProgress.value.repetitions) + exerciseInProgress.value.countdownDuration * 1000;
+
+  const pause = exerciseInProgress.value.pause;
+    console.log('Начало упражнения: ', Date.now())
+
+  setTimeout(() => {
+    if (!exerciseInProgress.value) return;
+    speechSynthesis.cancel();
+    console.info("----------")
+    console.info(`Конец выполнения упражнения: ${currentIndex.value}`, Date.now())
+
+      isPause.value = true;
+      startSpeach('Пауза');
+      utterance.onend = () => {}
+
+    if (currentIndex.value >= exercises.value.length) return;
+
+    setTimeout(() => {
+    console.info("Конец паузы упражнения: ", currentIndex.value)
+      // setCurrentExercise();
+
+      // speechSynthesis.cancel();
+
+      // startSpeach('зделай паузу');
+
+      speechSynthesis.cancel();
+
+      isPause.value = false;
+
+      pauseCountdown();
+
+      if (!exerciseInProgress.value) return;
+
+      startSpeach(`${exerciseInProgress.value.exerciseName}. ${exerciseInProgress.value.repetitions} повторений.`);
+
+      startPreparationExerciseSpeach();
+    }, pause);
+  }, timeCurrentEx);
+  
+  if (currentIndex.value === 1) {
+    console.log('Первый вызов startSpeach')
+    startSpeach(`${exerciseInProgress.value.exerciseName}. ${exerciseInProgress.value.repetitions} повторений`);
+    startPreparationExerciseSpeach();
+  }
+
+  console.info('Конец выполнения watch для упражнения: ', currentIndex.value)
+
 });
 </script>
 
@@ -445,20 +480,14 @@ onMounted(() => {
     </v-main>
     <audio ref="backgroundAudio" loop></audio>
 
-    <v-footer
-      app
-      fixed
-      class="d-flex flex-column"
-      elevation="3"
-      style="border-radius: 16px 16px 0 0"
-      :class="[
-        { 'bg-success text-white': isPlayerStarted },
-        { 'bg-warning text-white': playerState === 'paused' },
-      ]"
-    >
+    <v-footer app fixed class="d-flex flex-column" elevation="3" style="border-radius: 16px 16px 0 0" :class="[{'bg-success text-white': isPlayerStarted}, {'bg-warning text-white': playerState === 'paused'}]">
       <div class="pb-1 w-100">
         <div v-show="isPlayerStarted || isPlayerPaused" class="pb-2">
-          <p class="text-h5 mb-2">{{ playerTextDisplay }}</p>
+          <p class="text-h5 mb-2 d-flex justify-space-between align-center gap-2">
+            <span>{{ isPause && exerciseInProgress ? 'Пауза' : exerciseInProgress?.exerciseName }}</span>
+            <v-progress-circular :model-value="formattedCountdown" :rotate="360" :size="40" :width="5" color="white"></v-progress-circular>
+          </p>
+          
           <v-divider></v-divider>
         </div>
         <div v-if="playerState === 'idle' || playerState === 'reset'">
@@ -470,7 +499,7 @@ onMounted(() => {
             density="comfortable"
             class="mx-2"
             @click="startPlayer"
-            :disabled="isStartButtonDisabled"
+ :disabled="isStartButtonDisabled"
           >
             <v-icon>mdi-play</v-icon>
           </v-btn>
@@ -521,10 +550,8 @@ onMounted(() => {
         </div>
       </div>
       <div class="w-100"><v-divider></v-divider></div>
-      <div class="pt-2 text-caption w-100 d-flex justify-space-between">
-        <p v-if="!isPlayerStarted && !isPlayerPaused">
-          Выбрано упражнений: {{ totalSelectedExercises }}
-        </p>
+      <div class="pt-2 text-caption w-100 d-flex justify-space-between" >
+        <p v-if="!isPlayerStarted && !isPlayerPaused">Выбрано упражнений: {{ totalSelectedExercises }}</p>
         <p v-else>Оставшееся время: {{ formattedCountdown }}</p>
         <v-divider vertical class="mx-1"></v-divider>
         <p>Общее время: {{ formatTime(totalExercisesDuration) }}</p>
