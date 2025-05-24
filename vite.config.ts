5b60517f9cc67f97c6d161475ae5672b70966b4a
avtandil <shainidze@gmail.com>
2025-05-24 10:05:20 +0300

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,30 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
       manifest: {
-        name: 'My Awesome App',
-        short_name: 'Awesome App',
-        description: 'My Awesome App description',
+        name: 'Exercise Audio Couch',
+        short_name: 'Exercise Audio Couch',
+        description: 'Exercise Audio Couch',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         icons: [
           {
             src: 'pwa-192x192.png', // Replace with your icon path
             sizes: '192x192',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
-});
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
